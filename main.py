4c22674a63c42a72f05f7d5d2f51ef35b5077ba0
DevAtDawn <86164137+DevAtDawn@users.noreply.github.com>
2022-01-13 03:18:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import cv2 as cv
 import tempfile
 
 f = st.file_uploader("Upload file")
-tfile = tempfile.NamedTemporaryFile(delete=False)
+
+tfile = tempfile.NamedTemporaryFile(delete=False) 
 tfile.write(f.read())
+
+
 vf = cv.VideoCapture(tfile.name)
+
+stframe = st.empty()
+
+while vf.isOpened():
+    ret, frame = vf.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    stframe.image(gray)
